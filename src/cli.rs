43abf092fc3991eb7f65808524e538e26147f345
Rareș Cosma <rares@getbetter.ro>
2024-09-30 22:28:24 +0300

    cli: improve error handling

--- src/cli.rs
+++ src/cli.rs
@@ -1,7 +1,8 @@
 use std::path::PathBuf;
 
-use lexopt::prelude::*;
+use lexopt::{prelude::*, Error};
 
+#[derive(Copy, Clone)]
 pub enum Mode {
     Up,
     Down,
@@ -25,35 +26,36 @@ pub fn help() {
     std::process::exit(0);
 }
 
-pub fn parse_opts() -> Result<Opts, lexopt::Error> {
+pub fn parse_opts() -> Result<Opts, Error> {
     let def_path = PathBuf::from(default_path());
-    let mut mode = None;
-    let mut start_path = Some(def_path);
-    let mut num_steps = Some(DEFAULT_NUM_STEPS);
+    let mut mode = Err(Error::from("missing mode"));
+    let mut start_path = Ok(def_path);
+    let mut num_steps = Ok(DEFAULT_NUM_STEPS);
 
     let mut parser = lexopt::Parser::from_env();
     while let Some(arg) = parser.next()? {
         match arg {
             Value(val) => {
                 if val == "up" {
-                    mode = Some(Mode::Up);
+                    mode = Ok(Mode::Up);
                 } else if val == "down" {
-                    mode = Some(Mode::Down)
+                    mode = Ok(Mode::Down)
+                } else {
+                    mode = Err(Error::from(format!("invalid mode: {:?}", val)))
                 }
             }
             Short('p') | Long("path") => {
-                start_path = parser.value().ok().map(PathBuf::from);
+                start_path = parser.value().map(PathBuf::from);
             }
             Short('n') | Long("num-steps") => {
-                num_steps = parser.value().ok().and_then(|v| v.parse::<u64>().ok());
+                num_steps = parser.value().and_then(|v| {
+                    v.parse::<u64>()
+                        .map_err(|e| Error::from(format!("invalid number of steps: {e}")))
+                });
             }
             Short('h') | Long("help") => help(),
             _ => return Err(arg.unexpected()),
         }
     }
-    Ok(Opts {
-        mode: mode.ok_or("invalid mode")?,
-        start_path: start_path.ok_or("invalid path")?,
-        num_steps: num_steps.ok_or("invalid number of steps")?,
-    })
+    Ok(Opts { mode: mode?, start_path: start_path?, num_steps: num_steps? })
 }
